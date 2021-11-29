ac675fe9f3df2576fda684d130d7e8ddf5b84b77
unknown <elizadascal@gmail.com>
2021-11-29 10:56:09 +0000

    adding the components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,21 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("it is the postcode element visible", () => {
+  render(<App />);
+  const postcode = screen.getByTitle("postcodeInput");
+  expect(postcode).toBeInTheDocument();
+
+});
+
+// and so on
+// test ("if there is a bad postcode"), () => {
+// }
+
+// test ("it is invalid imput bad postcode the error is visible") , () => {
+// }
+
+// test ("it is the api call properly call "), () => {
+//
+// }
